be5af302339a994aa7e1296323dd16960a5c061a
Gugan M R <m.r.gugan24@gmail.com>
2025-12-09 15:05:16 +0530

    javascript

--- function.js
+++ function.js
@@ -4,3 +4,11 @@ function add(a, b) {
 
 console.llog(add(10, 20));
 console.log("---------------");
+
+//anonymous function
+var sub = function (a,b) {
+    return b - a;
+};
+
+console.log(sub(10,20));
+console.log("---------------");
