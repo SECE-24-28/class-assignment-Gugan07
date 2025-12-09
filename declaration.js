a66c2d56b5f3d255453d5df4c1508ee9e6780e4e
Gugan M R <m.r.gugan24@gmail.com>
2025-12-09 15:36:37 +0530

    const

--- declaration.js
+++ declaration.js
@@ -11,3 +11,27 @@ var b = a;
  let c = 10;
 c=20;
 console.log("c",c);
+
+//const
+const d = 10;
+//d = 20
+ //const d = 30;
+ console.log("d",d);
+
+ let person = {
+    fname : "harry",
+    lname : "potter",
+    age : 23,
+    address : {
+        city : "hogwarts",
+        pincode :696969,
+    },
+ };
+
+ const { fname, lname, age, address } = person;
+ const { city, pincode } = person.address;
+ 
+
+ console.log(person.fname);
+ console.log(person.age);
+ console.log(person.address.pincode);
