fc9621bdbf6a45216476b37a1e74c680b9d393cb
Gugan M R <m.r.gugan24@gmail.com>
2025-12-09 15:03:37 +0530

    javascript

---
+++ function.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.llog(add(10, 20));
+console.log("---------------");
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+
+console.log("---------------");
+
+for (var a of arr) {
+    console.log(a);
+}
+
+var person = {
+    fname: "harry",
+    lname: "potter",
+    age: 23,
+};
