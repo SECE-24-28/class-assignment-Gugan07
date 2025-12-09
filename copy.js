cabd5db9805433fe15652f8d827acc20b1d84329
Gugan M R <m.r.gugan24@gmail.com>
2025-12-09 15:55:12 +0530

    rest operator

--- copy.js
+++ copy.js
@@ -9,8 +9,19 @@
  };
 
  //let personCopy = person;//normal copy
-let personCopy = {...person};//shallow copy
+//let personCopy = {...person, address : { ...person.address} };//shallow copy it will copy only first level properties
+let personCopy = JSON.parse(JSON.stringify(person));//deep copy it will copy all level properties
 person.age = 34;
-person.address,city = "mumbai";
+person.address.city = "mumbai";
+
+
  console.log("person", person);
  console.log("personCopy", personCopy);
+
+ //rest operator
+
+ function add(a,b, ...num) {
+    return a + b;
+ }
+
+ console.log(add(10,30,56,78,56,34,34,84,7,3,32));
