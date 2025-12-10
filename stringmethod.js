11b2e733052c2da3f4a65a2695e4828063cbacdc
Gugan M R <m.r.gugan24@gmail.com>
2025-12-10 15:09:47 +0530

    string methods

--- stringmethod.js
+++ stringmethod.js
@@ -13,3 +13,17 @@ console.log(str1.endsWith("ld"));
 console.log(str1.replace("l", "i"));
 console.log(str1.replaceAll("l", "i"));
 console.log(str1.concat("sdfghjk"));
+
+let str2 ="         welcome                ";
+
+console.log(str2.trim());
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
+
+console.log(str1.repeat(3));
+
+console.log(str1.split(" "));
+
+let str3= "john";
+console.log(str3.padStart(10, "wer"));
+console.log(str3.padEnd(10, " "));
