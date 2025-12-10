0fd989a5acf567eada6fcdc4103b22da0e9b31f3
Gugan M R <m.r.gugan24@gmail.com>
2025-12-10 15:49:38 +0530

    string methods task

--- task.js
+++ task.js
@@ -13,7 +13,15 @@ let longest = str4.split(" ").reduce((a, b) => a.length >= b.length ? a : b);
 console.log(longest);
 
 let str5 = "john Doe Smith";
-console.log(str5.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" "));
+let initials = str5.split(" ").map(word => word.charAt(0).toUpperCase())  .join("");     
+console.log(initials); 
 
-
+let str6 = "programming";
+let result ="";
+for(let char in str6) {
+    if(!result.includes(str6(char))) {
+        result +=char;
+    }
+}
+console.log(result);
 
