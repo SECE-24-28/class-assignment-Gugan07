fe51b0aa64fa64c2839dbe9d8c623287c337d21b
Gugan M R <m.r.gugan24@gmail.com>
2025-12-10 15:23:18 +0530

    string methods task

---
+++ task.js
@@ -0,0 +1,8 @@
+let str1 = "hello world ";
+console.log(str1.replace("hello world", "Hello World"))
+
+let str2 = "i love javascript";
+let wordCount = str.split(" ").length;
+console.log(wordCount);
+
+
