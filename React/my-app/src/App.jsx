5c157e9606061c08e9114ed213305acb3125d696
Gugan M R <m.r.gugan24@gmail.com>
2025-12-12 15:29:40 +0530

    Initial commit - add project files (flattened nested repos)

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let fName = "Jhon";
+ return (
+  <>
+   <h1>Welcome to React JS</h1>
+   <h2>hello,{fName}</h2>
+   <button>Click Me</button>
+  </>
+ );
+}
+export default App
