08f0cad0808cab9f9749124a485030eccee13abf
Gugan M R <m.r.gugan24@gmail.com>
2025-12-12 15:48:16 +0530

    react basics

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -4,12 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let fName = "Jhon";
+ // let fName = "Jhon";
+ const [fName, setName] = useState("Jhon");
+ const [gift, setGift] = useState("Watch");
+ const [data, setdata] = useState([]);
+
+ const giftFromChild = (data) => {
+   setdata(data);
+   
+ };
+
  return (
   <>
    <h1>Welcome to React JS</h1>
+   {data.map((val,ind) =>
+   return <li>{val}</li>;
+   
+  )}
    <h2>hello,{fName}</h2>
-   <button>Click Me</button>
+   <button onClick={() => setfName("john")}>
+   change Name </button>
   </>
  );
 }
