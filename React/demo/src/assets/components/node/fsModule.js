a5c9891517877c6ab5d01d77ba2869717047c373
Gugan M R <m.r.gugan24@gmail.com>
2025-12-18 15:36:43 +0530

    fsModule.js

--- React/demo/src/assets/components/node/fsModule.js
+++ React/demo/src/assets/components/node/fsModule.js
@@ -9,4 +9,13 @@ fs.mkdirSync(folderPath);
 }else{
     console.log("folder already exists");
 }
-//fs.writeFileSync(folderPath,"This is a new File");
+fs.writeFileSync(folderPath,"This is a new File");
+
+fs.appendFileSync(folderPath," \n This is appended text");
+
+const res = fs.readFileSync(folderPath, "utf-8");
+console.log(res);
+
+fs.renameSync(fileURLToPath,"demo/rename.txt");
+
+fs.unlinkSync(renamedPth);
