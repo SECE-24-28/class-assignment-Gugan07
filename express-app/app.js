b0abf46ad897921e9568da2c3b75ecfd231b471b
Gugan M R <m.r.gugan24@gmail.com>
2025-12-22 15:53:28 +0530

    types of middleware

--- express-app/app.js
+++ express-app/app.js
@@ -12,11 +12,19 @@ const items = [
       itemname:'pencil',
       price:5 
     }
-]
+];
+
+app.use(req,res,next=>{
+    console.log('this is a middleware');
+    removeEventListener.currentTime = new Date();
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Welcome to the Express.js server!");
 });
-app.get("api/items", (req, res) => {
+
+app.get("/api/items/:id", (req, res) => {
     const id = req.params.id;
     const item = items.find((item) => item.id === parseInt(id));
     res.send(item);
