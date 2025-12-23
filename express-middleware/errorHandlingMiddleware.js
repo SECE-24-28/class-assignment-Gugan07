6b059a9748c733a060d2fc4acba0fbb04ddb3442
Gugan M R <m.r.gugan24@gmail.com>
2025-12-23 15:13:11 +0530

    throw an middleware error

--- express-middleware/errorHandlingMiddleware.js
+++ express-middleware/errorHandlingMiddleware.js
@@ -4,9 +4,14 @@ const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Error Handling Middleware Example!');
+    //res.send('Welcome to the Error Handling Middleware Example!');
+    
+throw Error("This is an Error");
 });
-
+app.use ((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke! ' + err.message);
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
